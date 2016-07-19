ee9ca1c99b44bb62cbbc465a5e299d7b1c2c141d
dongdong <liudongdong_922@163.com>
2016-07-19 19:32:37 +0800

    修改状态栏实现方式

--- app/src/main/java/com/mantoto/property/myapplication/activities/BaseActivity.java
+++ app/src/main/java/com/mantoto/property/myapplication/activities/BaseActivity.java
@@ -1,19 +1,16 @@
 package com.mantoto.property.myapplication.activities;
 
-import android.annotation.SuppressLint;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.AbsListView;
-import android.widget.AbsSpinner;
-import android.widget.AdapterView;
-import android.widget.ImageView;
 
-import com.mantoto.property.myapplication.utils.ScreenUtil;
+import com.mantoto.property.myapplication.R;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * Created by Mr.liu
@@ -22,17 +19,13 @@ import com.mantoto.property.myapplication.utils.ScreenUtil;
  * My Application
  */
 public class BaseActivity extends FragmentActivity {
-
     private static final String TAG = "BaseActivity";
-
-    public static String apkName;
-
-    protected boolean isStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        isStart = true;
+        //设置状态栏颜色
+        initSystemBar(this);
     }
 
     @Override
@@ -52,52 +45,29 @@ public class BaseActivity extends FragmentActivity {
 
     @Override
     protected void onDestroy() {
-        isStart = false;
         super.onDestroy();
     }
 
-    protected void unbindDrawables(View view) {
-        if (view != null) {
-            if (view.getBackground() != null) {
-                view.getBackground().setCallback(null);
-            }
-            if (view instanceof ImageView) {
-                ImageView imageView = (ImageView) view;
-                imageView.setImageDrawable(null);
-            }
-            if (view instanceof ViewGroup && !(view instanceof AdapterView)) {
-                for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
-                    unbindDrawables(((ViewGroup) view).getChildAt(i));
-                }
-                if (!(view instanceof AbsSpinner) && !(view instanceof AbsListView)) {
-                    ((ViewGroup) view).removeAllViews();
-                }
-            }
-        }
-    }
-
-
-    @SuppressLint("InlinedApi")
-    protected void setImmerseLayout(View view) {
+    public static void initSystemBar(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window window = getWindow();
-            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-            int statusBarHeight = ScreenUtil.getStatusBarHeight(this.getBaseContext());
-            view.setPadding(0, statusBarHeight, 0, 0);
+            setTranslucentStatus(activity, true);
         }
+        SystemBarTintManager tintManager = new SystemBarTintManager(activity);
+        tintManager.setStatusBarTintEnabled(true);
+        //使用主色调
+        tintManager.setStatusBarTintResource(R.color.main_color);
     }
 
-    @SuppressLint("InlinedApi")
-    protected void setImmerseLayout() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window window = getWindow();
-            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    @TargetApi(19)
+    private static void setTranslucentStatus(Activity activity, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
         }
+        win.setAttributes(winParams);
     }
-
 }
--- app/src/main/java/com/mantoto/property/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/mantoto/property/myapplication/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mantoto.property.myapplication.activities;
 
 import android.os.Bundle;
-
 import com.mantoto.property.myapplication.R;
 
 public class MainActivity extends BaseActivity {
@@ -10,6 +9,5 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setImmerseLayout(findViewById(R.id.weather));
     }
 }
