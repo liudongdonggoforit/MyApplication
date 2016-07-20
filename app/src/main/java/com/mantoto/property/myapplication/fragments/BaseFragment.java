ab45a3b4b7f5b0a0f3397e0da07914640b85ff22
liudongdong <liudongdong_922@163.com>
2016-07-21 07:51:40 +0800

    添加BaseActivity 和 BaseFragment

--- app/src/main/java/com/mantoto/property/myapplication/fragments/BaseFragment.java
+++ app/src/main/java/com/mantoto/property/myapplication/fragments/BaseFragment.java
@@ -1,6 +1,11 @@
 package com.mantoto.property.myapplication.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Mr.liu
@@ -9,4 +14,32 @@ import android.app.Fragment;
  * My Application
  */
 public class BaseFragment extends Fragment {
+    protected View mView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViews();
+        initEvents();
+        onLoadData();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return mView;
+    }
+
+    protected void onLoadData() {
+    }
+
+    protected void initEvents() {
+    }
+
+    protected void initViews() {
+    }
 }
