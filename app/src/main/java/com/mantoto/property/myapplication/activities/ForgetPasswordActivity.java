07573a0b37e5107a72cb1c1b78392b6e839d54b0
liudongdong <liudongdong_922@163.com>
2016-08-01 00:52:45 +0800

    添加登录 、注册 、忘记密码页面 书写统一背景色切换

---
+++ app/src/main/java/com/mantoto/property/myapplication/activities/ForgetPasswordActivity.java
@@ -0,0 +1,20 @@
+package com.mantoto.property.myapplication.activities;
+
+import android.view.View;
+
+import com.mantoto.property.myapplication.R;
+
+/**
+ * Created by liudongdong on 2016/8/1.
+ */
+public class ForgetPasswordActivity extends BaseActivity implements View.OnClickListener{
+    @Override
+    protected int getContentViewResId() {
+        return R.layout.activity_forget_password;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
