5b0be8dcdfb109e83a4315c59493398942493fc6
liudongdong <liudongdong_922@163.com>
2016-08-13 22:43:59 +0800

    同步代码

---
+++ app/src/main/java/com/mantoto/property/myapplication/adapter/CateringAdapter.java
@@ -0,0 +1,24 @@
+package com.mantoto.property.myapplication.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by liudongdong on 2016/8/12.
+ */
+public class CateringAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
